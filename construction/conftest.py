91dadd0ffee6e328c0c411c7c7697debaf7b5106
Sabeenirfan <133214650+Sabeenirfan@users.noreply.github.com>
2025-06-13 16:25:34 +0500

    Create conftest.py

---
+++ construction/conftest.py
@@ -0,0 +1,14 @@
+# conftest.py
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture
+def browser():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
