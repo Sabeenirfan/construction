dfe18cbf2a4ad0547d0dc282821bec5be05b30f0
Sabeenirfan <133214650+Sabeenirfan@users.noreply.github.com>
2025-06-13 20:31:04 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,13 +2,15 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 @pytest.fixture
 def browser():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--user-data-dir=/tmp/chrome-user-data")
+    
+    service = webdriver.chrome.service.Service('/usr/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
